95a9693284758892ceefcc64a145f9b726d38162
jtigik <jeftigik@hotmail.com>
2025-10-06 22:26:30 -0300

    Implementing Methods to MainActivity.java

--- App_02/app/src/main/java/br/com/jtigik/app_02/MainActivity.java
+++ App_02/app/src/main/java/br/com/jtigik/app_02/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btnListarPessoa;
     private Button btnRemoverPessoa;
     private Button btnSair;
+
+    private TextView txtCriarPessoa;
     private TextView txtPessoa;
+    private TextView txtIdade;
+
     int contador = 0;
     int eventos = 0;
 
@@ -39,41 +44,40 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        iniciarComponentesDeLayout();
+
+        btnFabricarPessoa.setOnClickListener(v -> { fabricaPessoa(); });
+
+        btnCriarPessoa.setOnClickListener(v -> { criarPessoa(); });
+
+        btnEditarPessoa.setOnClickListener(v -> { editarPessoa(); });
+
+        btnListarPessoa.setOnClickListener(v -> { listarPessoas(); });
+
+        btnRemoverPessoa.setOnClickListener(v -> { removerPessoa(); });
+
+        btnSair.setOnClickListener(v -> { sairDoApp(); });
+    }
+
+    private void iniciarComponentesDeLayout() {
         this.btnFabricarPessoa = findViewById(R.id.btnFabricarPessoa);
         this.btnCriarPessoa = findViewById(R.id.btnCriarPessoa);
         this.btnEditarPessoa = findViewById(R.id.btnEditarPessoa);
         this.btnListarPessoa = findViewById(R.id.btnListarPessoa);
         this.btnRemoverPessoa = findViewById(R.id.btnRemoverPessoa);
         this.btnSair = findViewById(R.id.btnSair);
+        this.txtCriarPessoa = findViewById(R.id.txtCriarPessoa);
         this.txtPessoa = findViewById(R.id.txtPessoa);
-
-        btnFabricarPessoa.setOnClickListener(v -> {
-            fabricaPessoa();
-        });
-
-        btnCriarPessoa.setOnClickListener(v -> {
-            criarPessoa();
-        });
-
-        btnEditarPessoa.setOnClickListener(v -> {
-            editarPessoa();
-        });
-
-        btnListarPessoa.setOnClickListener(v -> {
-            listarPessoas();
-        });
-
-        btnRemoverPessoa.setOnClickListener(v -> {
-            removerPessoa();
-        });
-
-        btnSair.setOnClickListener(v -> {
-            contador++;
-            Log.d("eventos", "Buttom 'Sair' clicado "+contador);
-            finish();
-        });
+        this.txtIdade = findViewById(R.id.txtIdade);
     }
 
+    private void sairDoApp() {
+        contador++;
+        Log.d("eventos", "Buttom 'Sair' clicado "+contador);
+        finish();
+    }
+
+    @SuppressLint("SetTextI18n")
     private void fabricaPessoa(){
         txtPessoa.setText("Número de pessoas criadas: " + contador);
 
@@ -84,13 +88,23 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     private void criarPessoa(){
-        eventos ++;
+        pessoa = new Pessoa();
+        pessoa.setNome("João");
+        pessoa.setSobrenome("Silva");
+        pessoa.setIdade(getIdade());
+
+        txtPessoa.setText(pessoa.toString());
+        txtIdade.setText(String.valueOf(pessoa.getIdade()));
+
+        Toast.makeText(this, "Pessoa Criada", Toast.LENGTH_SHORT).show();
+
         contador++;
-        fabricaPessoa();
-        txtPessoa.setText(pessoa.getNome()+" "+pessoa.getSobrenome()+
-                " "+pessoa.getIdade()+" - "+contador+ " - Pessoa(s)");
-        Log.e("eventos", "Total eventos: "+eventos + " Criar Pessoa");
-        Log.w("eventos", "Total eventos: "+eventos + " Criar Pessoa");
+        txtCriarPessoa.setText("Número de pessoas criadas: " + contador);
+
+        Log.d("eventos", "Buttom 'Criar Pessoa' clicado "+contador);
+        Log.i("eventos", "Nova Pessoa criada:  "+pessoa.toString());
+        Log.w("eventos", "Nome da Pessoa:  "+pessoa.getNome());
+        Log.v("eventos", "Idade da Pessoa:  "+pessoa.getIdade());
     }
 
     @SuppressLint("SetTextI18n")
@@ -104,11 +118,24 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     private void editarPessoa(){
-        eventos ++;
-        contador++;
-        txtPessoa.setText(pessoa.getNome()+ " "+pessoa.getSobrenome()+
-                " "+pessoa.getIdade()+" - "+contador+ " - Pessoa(s)");
-        Log.d("eventos", "Total eventos: "+eventos + " Editar Pessoa");
+
+        if(pessoa != null){
+            Toast.makeText(this, String.format("%d Pessoa encontrada", contador), Toast.LENGTH_SHORT).show();
+            pessoa.setNome("Maria");
+            pessoa.setSobrenome("Silva");
+            pessoa.setIdade(getIdade());
+            txtPessoa.setText(pessoa.toString());
+            txtIdade.setText(String.valueOf(pessoa.getIdade()));
+            Log.i("eventos", "Pessoa editada:  "+pessoa.toString());
+        }else{
+            Toast.makeText(this, "Pessoa não encontrada", Toast.LENGTH_SHORT).show();
+        }
+
+//        eventos ++;
+//        contador++;
+//        txtPessoa.setText(pessoa.getNome()+ " "+pessoa.getSobrenome()+
+//                " "+pessoa.getIdade()+" - "+contador+ " - Pessoa(s)");
+//        Log.d("eventos", "Total eventos: "+eventos + " Editar Pessoa");
     }
     @SuppressLint("SetTextI18n")
     private void removerPessoa(){
